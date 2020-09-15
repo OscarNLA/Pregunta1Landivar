3b58e0c29e4196a36ebcba4c71560fbe8819836a
OscarNLA <a2019114151@estudiantes.upsa.edu.bo>
2020-09-15 14:47:51 -0400

    Tarea Terminada

--- Pr1LandivarAPI/UnitTestLandivar/UnitTest1.cs
+++ Pr1LandivarAPI/UnitTestLandivar/UnitTest1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using System.Net;
 using System.Web.Http;
-using System.Web.Http.Controllers;
 using System.Web.Http.Results;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pr1LandivarAPI.Controllers;
 using Pr1LandivarAPI.Models;
 
-namespace UnitTestLandivar
+namespace UnitTestPregunta1
 {
     [TestClass]
     public class UnitTest1
@@ -14,38 +14,85 @@ namespace UnitTestLandivar
         [TestMethod]
         public void TestMethodGet()
         {
-            //arrange
-            LandivarsController landivarsController = new LandivarsController();
-            //act
-            var Resultado = landivarsController.GetLandivars();
-            //assert
-            Assert.IsNotNull(Resultado);
+            //Arrange
+            LandivarsController landivarController = new LandivarsController();
 
+            //Act
+            var Resultado = landivarController.GetLandivars();
+
+            //Assert
+            Assert.IsNotNull(Resultado);
         }
+
         [TestMethod]
         public void TestMethodPost()
         {
-            //arrange
-            LandivarsController landivarsController = new LandivarsController();
+            //Arrange
+            LandivarsController landivarController = new LandivarsController();
             Landivar Esperado = new Landivar()
             {
                 LandivarID = 1,
-                FriendofLandivar = "Oscar Landivar",
+                FriendofLandivar = "Pedro",
                 Place = Places.Santa_Cruz,
-                Email = "oscarnlandivara@gmail.com",
-                Birthdate = DateTime.Now
-
+                Email = "pedro@hotmail.com",
+                Birthdate = DateTime.Today
             };
-            //act
-            var Resultado = landivarsController.PostLandivar(Esperado);
+
+            //Act
+            var Resultado = landivarController.PostLandivar(Esperado);
             var creado = Resultado as CreatedAtRouteNegotiatedContentResult<Landivar>;
 
-            //assert
+            //Assert
             Assert.IsNotNull(creado);
             Assert.AreEqual("DefaultApi", creado.RouteName);
-            Assert.IsNotNull(creado.RouteValues["id"]);
-
+            Assert.AreEqual(Esperado.FriendofLandivar, creado.Content.FriendofLandivar);
         }
 
+        [TestMethod]
+        public void TestMethodPut()
+        {
+            //Arrange
+            LandivarsController landivarController = new LandivarsController();
+            Landivar landivar = new Landivar()
+            {
+                LandivarID = 1,
+                FriendofLandivar = "Victor",
+                Place = Places.Santa_Cruz,
+                Email = "victor@hotmail.com",
+                Birthdate = DateTime.Today
+            };
+
+            //Act
+            IHttpActionResult actionResultPost = landivarController.PostLandivar(landivar);
+            var result = landivarController.PutLandivar(landivar.LandivarID, landivar) as StatusCodeResult;
+
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(StatusCodeResult));
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void TestMethodDelete()
+        {
+            //Arrange
+            LandivarsController landivarController = new LandivarsController();
+            Landivar landivar = new Landivar()
+            {
+                LandivarID = 1,
+                FriendofLandivar = "Enrique",
+                Place = Places.Santa_Cruz,
+                Email = "enrique@hotmail.com",
+                Birthdate = DateTime.Today
+            };
+
+            //Act
+            IHttpActionResult actionResultPost = landivarController.PostLandivar(landivar);
+            IHttpActionResult actionResultDelete = landivarController.DeleteLandivar(landivar.LandivarID);
+
+            //Assert
+            Assert.IsInstanceOfType(actionResultDelete, typeof(OkNegotiatedContentResult<Landivar>));
+        }
     }
 }
